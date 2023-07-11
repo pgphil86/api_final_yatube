d6b838a70f1cd6c76585dc238b35049093c465e8
Павел Филиппов <pgphil@yandex.ru>
2023-07-11 10:04:07 +0300

    In view del user in follow.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -57,8 +57,7 @@ class FollowViewSet(CreateListViewSet):
     search_fields = ('following__username',)
 
     def get_queryset(self):
-        user = self.request.user
-        return user.follower.all()
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
